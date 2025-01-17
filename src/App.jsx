55c83bc37013cfa76828bf58af1ad5f6401a8e56
Nelson Juarez Rivas <najr91@gmail.com>
2025-01-17 15:26:20 -0300

    Creamos estructura de carpetas

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Proyecto Gammer</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
