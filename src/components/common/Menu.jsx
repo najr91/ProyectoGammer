d25ddca6007017ee568eaa044253862314dd7545
Nelson Juarez Rivas <najr91@gmail.com>
2025-01-20 17:01:27 -0300

    agregamos estilos responsive al nav y a las card de productos

---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,21 @@
+import { Navbar,Nav,Container } from "react-bootstrap";
+import '../../style/Menu.css'
+
+const Menu = () => {
+    return (
+        <Navbar expand="lg" className="Menu navbar-dark">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+};
+
+export default Menu;
