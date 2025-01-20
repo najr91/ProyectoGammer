f73e45249f978c639d4422b4069bf773864f4e4b
Matias <matiassanchez13422@gmail.com>
2025-01-20 16:28:02 -0300

    Se agrego la carpta common y se creo un archivo llamado footer para maquetar este

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './components/pages/Inicio';
+import Footer from './components/pages/common/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
          <Routes>
           <Route path='/' element={<Inicio></Inicio>}></Route>
          </Routes>
+         <Footer/>
         </BrowserRouter>
       </div>
       
---
+++ src/components/pages/common/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <section>
+        <div className="app-container">
+  <main className="content">
+  </main>
+  <footer className="footer">
+    <p>© Todos los derechos reservados</p>
+  </footer>
+</div>
+    </section>
+    );
+};
+
+export default Footer;
