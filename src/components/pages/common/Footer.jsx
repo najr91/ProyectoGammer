e5bd0520710eafa4ada461de7cf788f95a2eaaac
Matias <matiassanchez13422@gmail.com>
2025-01-20 16:50:42 -0300

    Se descargo react-bootstarp-icon para poder agregar iconos al footer

--- src/components/pages/common/Footer.jsx
+++ src/components/pages/common/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../../style/Style.css'
 
+
 const Footer = () => {
     return (
         <section>
@@ -8,7 +9,11 @@ const Footer = () => {
   <main className="content">
   </main>
   <footer className="footer">
-    <p>© Todos los derechos reservados</p>
+    <div className='col-12 col-md-3 col-lg-3 fs-3 p-3 '><b>Contactanos</b></div>
+
+    <div className='col-12 col-md-3 col-lg-3'></div>
+
+    <div className='col-12 col-md-3 col-lg-3'></div>
   </footer>
 </div>
     </section>
