425270a9651823f0bd8b9dd29abf7e870954d51c
Matias <matiassanchez13422@gmail.com>
2025-01-20 17:50:49 -0300

    Se agregos las otras secciones del footer y se cambio su estilo

--- src/components/pages/common/Footer.jsx
+++ src/components/pages/common/Footer.jsx
@@ -1,41 +1,75 @@
 import React from "react";
 import "../../../style/Style.css";
-import { Facebook, Instagram, Whatsapp } from "react-bootstrap-icons";
+import { Clock, Facebook, Instagram, Whatsapp } from "react-bootstrap-icons";
 
 const Footer = () => {
   return (
     <section>
       <div className="app-container">
         <main className="content"></main>
-        <footer className="footer">
-          <div className="col-12 col-md-3 col-lg-3 fs-3 p-3 ">
-            <ul className="list-unstyled">
-              <li>
-                <p>
-                  <b>Contactanos</b>
-                </p>
-              </li>
-              <li>
-                <p>
+        <footer className="footer ">
+          <div className="row mx-3">
+            <div className="col-12 col-md-3 col-lg-3 fs-4 p-2 ms-3">
+              <ul className="list-unstyled">
+                <li>
+                  <p>
+                    <b>CONTACTANOS</b>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <b>
+                      <a href="https://www.facebook.com " target="_blank">
+                        <Facebook className="me-3  " />
+                      </a>
+                      <a href="https://www.instagram.com/  " target="_blank">
+                        <Instagram className="me-3 " />
+                      </a>
+                      <a
+                        href="https://www.whatsapp.com/?lang=es_LA "
+                        target="_blank"
+                      >
+                        <Whatsapp className="" />
+                      </a>
+                    </b>
+                  </p>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-12 col-md-3 col-lg-3 mx-auto p-2">
+              <ul className="list-unstyled">
+                <li className="fs-4">
                   <b>
-                    <a href="https://www.facebook.com " target="_blank">
-                      <Facebook className="me-3  text-white" />
-                    </a>
-                    <a href="https://www.instagram.com/  " target="_blank">
-                      <Instagram className="me-3 text-white" />
-                    </a>
-                    <a href="https://www.whatsapp.com/?lang=es_LA " target="_blank">
-                      <Whatsapp className="text-white"   />
-                    </a>
+                    <p>INCONVENIENTE</p>
                   </b>
-                </p>
-              </li>
-            </ul>
+                </li>
+                <li className="fs-5 row">
+                  <p>
+                    Por cualquier problema o duda dirigase <a href="">aquí</a>.
+                  </p>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-12 col-md-3 col-lg-3 ms-auto">
+              <ul className="list-unstyled">
+                <li className="fs-4">
+                  <b>
+                    <p>SERVICIO AL CLIENTE</p>
+                  </b>
+                </li>
+                <li>
+                  <p>
+                   <Clock/>
+                    SIEMPRE ABIERTO - horarios de despacho a partir de 11AM a 10PM. Los pedidos del domingo se envian el día lunes
+                  </p>
+                </li>
+              </ul>
+            </div>
+
+            <div className="mx-auto mt-3"><p><b>©Todos los derechos reservados</b></p></div>
           </div>
-
-          <div className="col-12 col-md-3 col-lg-3"></div>
-
-          <div className="col-12 col-md-3 col-lg-3"></div>
         </footer>
       </div>
     </section>
