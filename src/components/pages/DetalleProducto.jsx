02999131e9972a30fff739fd362483734445ceee
Matias <matiassanchez13422@gmail.com>
2025-01-22 19:46:59 -0300

    Se creo la primera parte de la card de detalleproducto

--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import "../../style/detalleProducto.css"
+import React from "react";
+import "../../style/detalleProducto.css";
 
 const DetalleProducto = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <section className="container mt-5">
+      <article className="row justify-content-center">
+        <div className="col-12 col-md-8">
+          <div className="card card-estilizada shadow-sm rounded">
+            <div className="row">
+              <div className="col-md-5">
+                <img
+                  src="https://assetsio.gnwcdn.com/final-fantasy-14-online-dawntrail.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default DetalleProducto;
