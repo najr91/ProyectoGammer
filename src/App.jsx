7f16637fd8e6ec57547babb72e219d4bae6694ef
Matias <matiassanchez13422@gmail.com>
2025-01-22 19:15:41 -0300

    Se arreglo la carpeta css y se agrego la rutas para dirigirse a la pagina de DetalleProducto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './components/pages/Inicio';
+import DetalleProducto from "./components/pages/DetalleProducto"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
          <Routes>
           <Route path='/' element={<Inicio></Inicio>}></Route>
+          <Route path='/DetalleProducto' element={<DetalleProducto></DetalleProducto>}></Route>
          </Routes>
         </BrowserRouter>
       </div>
--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const DetalleProducto = () => {
     return (
-        <div>
+        <section>
             
-        </div>
+        </section>
     );
 };
 
