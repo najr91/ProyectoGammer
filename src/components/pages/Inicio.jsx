5ccc3a99cf3332caff3a8ebc5ca41e308fedf9cf
Matias <matiassanchez13422@gmail.com>
2025-01-27 23:02:18 -0300

    Se agrego el link del error404 al learn more del inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,6 +3,7 @@ import "../../style/Inicio.css";
 import Productos from "../Productos/Productos";
 import SliderPortada from "../Slider/SliderPortada";
 import { getProductos } from "../helpers/queriesProductos";
+import { Link } from "react-router-dom";
 
 const Inicio = () => { 
   // creamos un estado para guardar los productos
@@ -28,7 +29,7 @@ const Inicio = () => {
                 vitae corrupti voluptatum perferendis accusamus quod officiis
                 illum laboriosam quo incidunt?
               </p>
-              <button className="button-portada">Learn More</button>
+              <Link className="button-portada text-decoration-none text-black" to={"*"}>Learn More</Link>
               </div>
             </div>
           </div>
