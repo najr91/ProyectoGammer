9437bbd9c3c84b70b6135699e93fdea286a26275
Matias <matiassanchez13422@gmail.com>
2025-01-28 00:49:12 -0300

    Se agrego el codigo para que el footer quede siempre abajo

--- src/components/pages/Carrito.jsx
+++ src/components/pages/Carrito.jsx
@@ -41,7 +41,7 @@ const Carrito = ({ carrito, setCarrito }) => {
 
   return (
     <section className="container mt-5">
-      <h3 className="text-center mb-4">Tu Carrito de Compras</h3>
+      <h3 className="text-center mb-4 mt-5">Tu Carrito de Compras</h3>
 
       <Row className="g-3">
         {carrito.length > 0 ? (
